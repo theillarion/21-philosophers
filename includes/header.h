612d814abef02cc5e2a36f0df2e6d0b258d21f4e
Illarion <pro100illarion@gmail.com>
2022-06-02 12:27:42 +0300

    Changed struct and added new prototype functions

--- includes/header.h
+++ includes/header.h
@@ -19,16 +19,16 @@ enum	e_state
 
 typedef struct s_philo
 {
-	size_t			id;
-	size_t			left_fork;
-	size_t			right_fork;
+	size_t	id;
+	size_t	left_fork;
+	size_t	right_fork;
 }			t_philo;
 
 typedef struct s_forks
 {
-	pthread_mutex_t	*forks;
+	pthread_mutex_t	*fork;
 	bool			*is_used;
-}		t_forks;
+}					t_forks;
 
 typedef struct s_settings
 {
@@ -39,23 +39,71 @@ typedef struct s_settings
 	size_t	time_to_sleep;
 	size_t	count_eat;
 	bool	is_end;
-}		t_settings;
+}			t_settings;
+
+typedef struct s_critical
+{
+	t_forks			*forks;
+	bool			is_end_game;
+	pthread_mutex_t	mutex;
+}					t_critical;
+
+typedef	struct s_status
+{
+	bool			is_die;
+	size_t			id;
+	t_time			time_die;
+	pthread_mutex_t	mutex_status;
+}					t_status;
 
 typedef struct s_main
 {
 	t_philo			*adrs_philo;
+	pthread_t		*adrs_threads;
+	t_status	*status;
+	void			*adrs_main_struct;
 	t_settings		*settings;
-	struct s_critical
-	{
-		t_forks			*forks;
-		pthread_mutex_t	mutex;
-	}	critical;
-}	t_main;
+	t_critical		*critical;
+}					t_main;
 
+//		debug.c
+void	print_philos(const t_philo	*philos, size_t	count);
+void	print_settings(const t_settings	*settings);
+void	print_critical(const t_critical	*critical, size_t count);
+void	print_main(const t_main	*main);
+
+//		utilities_int.c
+int		ft_isdigit(int symbol);
+int		ft_atoi(const char	*str);
+
+//		utilities_string.c
+size_t	ft_strlen(const char	*str);
+int		ft_strncmp(const char	*str1, const char	*str2, size_t n);
+
+//		exit.c
+void	ft_fail(t_main	*env, const char	*message_error, bool	is_clean);
+void	ft_success(t_main	*env);
+
+//		fill.c
+bool	ft_fill(t_main	**main, t_settings	*settings);
+
+//		initial.c
+void	ft_initial_env(t_main	*env);
+
+//		deinitial.c
+void	ft_smart_free(void	**memory);
+void	ft_deinitial_all(t_main	*_main);
+
+//		action.c
 void	*main_action(void	*data);
-void	ft_initial_philos(t_philo	**philos, size_t	count);
+
+//		validation.c
+bool	ft_read_args(t_settings	*settings, size_t argc, const char	**argv);
+
+//		check.c
 void	check(int	code);
-void	ft_main_initial(t_main	*main);
-int		main(void);
+
+//		main.c
+int		main(int argc, char	**argv);
 
 #endif
