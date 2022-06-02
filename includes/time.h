72d783f81bb83eafa20dc9034a059e4978f0b214
Illarion <pro100illarion@gmail.com>
2022-06-02 12:28:07 +0300

    Changed style code

--- includes/time.h
+++ includes/time.h
@@ -8,6 +8,7 @@
 typedef struct timeval t_time;
 typedef unsigned long long t_u64int;
 
+//			time.c
 t_u64int	GetDifferenceMs(const struct timeval	*time1, const struct timeval	*time2);
 t_u64int	GetDifferenceCurrentTimeMs(const t_time	*time);
 t_time		GetCurrentTime();
